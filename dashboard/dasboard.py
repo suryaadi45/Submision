57e289a4dd2928aff46db87adc826d6cef3581af
suryaadi45 <adisurya271@gmail.com>
2024-12-25 11:41:14 +0800

    Update dasboard.py

--- dashboard/dasboard.py
+++ dashboard/dasboard.py
@@ -3,7 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+"""## Data Wrangling
 
+### Gathering Data
+"""
 
 customers_df = pd.read_csv("https://raw.githubusercontent.com/suryaadi45/Submision/refs/heads/main/Data/customers_dataset.csv")
 customers_df.head()
@@ -14,6 +17,13 @@ orders_items_df.head()
 order_payments_df = pd.read_csv("https://raw.githubusercontent.com/suryaadi45/Submision/refs/heads/main/Data/order_payments_dataset.csv")
 order_payments_df.head()
 
+"""**Insight:**
+- Pengambilan data Costumers
+- Pengambilan data Order Items
+- Pengambilan data Order Payments
+
+### Assessing Data
+"""
 
 customers_df.info()
 
@@ -34,6 +44,13 @@ order_payments_df.isna().sum()
 print("Jumlah duplikasi: ", order_payments_df.duplicated().sum())
 order_payments_df.describe()
 
+"""**Insight:**
+- Data costumers tidak ada keanehan
+- Data orders_items terdapat nilai maksimal yang lebih tinggi dibanding lainnya di price
+- Data orders_payments terdapat nilai maksimal yang lebih tinggi dibanding lainnya di payment value
+
+### Cleaning Data
+"""
 
 orders_items_df[orders_items_df.price == orders_items_df.price.max()]
 
@@ -42,6 +59,13 @@ orders_items_df.groupby(by="price").agg({
     "price": ["max", "min", "mean", "std"]
 })
 
+orders_items_df.groupby(by="freight_value").agg({
+    "order_id": "nunique",
+    "freight_value": ["max", "min", "mean", "std"]
+})
+
+print("freight_value", "sum")
+
 order_payments_df[order_payments_df.payment_value == order_payments_df.payment_value.max()]
 
 order_payments_df.groupby(by="payment_value").agg({
@@ -49,6 +73,40 @@ order_payments_df.groupby(by="payment_value").agg({
     "payment_value": ["max", "min", "mean", "std"]
 })
 
+"""**Insight:**
+- Pengecekan dan Perapian Data Orders Items
+- Pengecekan dan Perapian Data Orders Payments
+
+## Exploratory Data Analysis (EDA)
+
+### Explore ...
+"""
+
+some_df = pd.merge(
+    left=orders_items_df,
+    right=order_payments_df,
+    how="left",
+    left_on="order_id",
+    right_on="order_id"
+)
+some_df.head()
+
+some_df.groupby(by=["seller_id", "product_id"]).agg({
+    "order_item_id": "sum",
+    "price": "sum"
+})
+
+some_df.groupby(by=["payment_type", "order_id"]).agg({
+        "payment_installments": "sum",
+        "payment_value": "sum"
+})
+
+some_df.groupby(by=["payment_type"]).agg({
+        "price": "sum",
+        "payment_value": "sum"
+})
+
+some_df.to_csv("some_data.csv", index=False)
 
 customers_df.describe(include="all")
 
@@ -72,6 +130,18 @@ order_payments_df.sample(5)
 
 order_payments_df.groupby(by="payment_type").order_id.nunique().sort_values(ascending=False)
 
+some_df.to_csv("some_data.csv", index=False)
+
+"""**Insight:**
+- Mendapatkan 5 costumer state dan costumer city terbanyak
+- Mendapatkan produk yang terlaris dan penjual yang paling banyak pembeli
+- Mendapatkan metode pembayaran terbanyak
+
+## Visualization & Explanatory Analysis
+
+### Pertanyaan 1:
+"""
+
 state_counts = customers_df.groupby('customer_state')['customer_unique_id'].nunique().sort_values(ascending=False).head(5)
 
 plt.figure(figsize=(12, 6))
@@ -92,6 +162,8 @@ plt.xlabel('City')
 plt.ylabel('Number of Customers')
 _ = plt.xticks(rotation=45, ha='right')
 
+"""### Pertanyaan 2:"""
+
 state_counts = orders_items_df.groupby('product_id')['order_id'].nunique().sort_values(ascending=False).head(5)
 
 plt.figure(figsize=(12, 6))
@@ -102,6 +174,7 @@ plt.xlabel('Product ID')
 plt.ylabel('Number of Orders')
 _ = plt.xticks(rotation=45, ha='right')
 
+"""### Pertanyaan 3:"""
 
 state_counts = orders_items_df.groupby('seller_id')['order_id'].nunique().sort_values(ascending=False).head(5)
 
@@ -113,6 +186,7 @@ plt.xlabel('Seller ID')
 plt.ylabel('Number of Orders')
 _ = plt.xticks(rotation=45, ha='right')
 
+"""### Pertanyaan 4:"""
 
 state_counts = order_payments_df.groupby('payment_type')['order_id'].nunique().sort_values(ascending=False).head(5)
 
@@ -124,3 +198,16 @@ plt.xlabel('Payment Type')
 plt.ylabel('Number of Payment')
 _ = plt.xticks(rotation=45, ha='right')
 
+"""**Insight:**
+- Menampilkan 5 daerah dengan pembeli terbanyak
+- Menampilkan 5 produk dengan pembelian terbanyak
+- Menampilkan 5 ID penjual barang terbanyak
+- Menampilkan 5 metode pembayaran
+
+## Conclusion
+
+- Costumer terbanyak berasal dari daerah SP yang paling banyak dari daerah Sao Paulo
+- Produk ID 9571759451b1d780ee7c15012ea109d4 memiliki penjualan terbanyak
+- Penjual dengan ID 2709af9587499e95e803a6498a5a56e9 memiliki penjualan terbanyak
+- Metode yang digunakan terbanyak dalam pembayaran menggunakan Credit Card
+"""

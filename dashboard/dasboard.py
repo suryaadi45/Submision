854802480e5ec39396b32d8676abf103153d04d3
suryaadi45 <adisurya271@gmail.com>
2024-12-26 12:52:09 +0800

    Update dasboard.py

--- dashboard/dasboard.py
+++ dashboard/dasboard.py
@@ -1,213 +1,148 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+import numpy as np
 
-"""## Data Wrangling
+sns.set(style='dark')
 
-### Gathering Data
-"""
+def create_customers_state_df(df):
+    customers_state_df = df.groupby('customer_state')['customer_unique_id'].nunique().sort_values(ascending=False).head(5)
 
-customers_df = pd.read_csv("https://raw.githubusercontent.com/suryaadi45/Submision/refs/heads/main/Data/customers_dataset.csv")
-customers_df.head()
+    return customers_state_df
 
-orders_items_df = pd.read_csv("https://raw.githubusercontent.com/suryaadi45/Submision/refs/heads/main/Data/order_items_dataset.csv")
-orders_items_df.head()
+def create_customers_city_df(df):
+    customers_city_df = df.groupby('customer_city')['customer_unique_id'].nunique().sort_values(ascending=False).head(5)
 
-order_payments_df = pd.read_csv("https://raw.githubusercontent.com/suryaadi45/Submision/refs/heads/main/Data/order_payments_dataset.csv")
-order_payments_df.head()
+    return customers_city_df
 
-"""**Insight:**
-- Pengambilan data Costumers
-- Pengambilan data Order Items
-- Pengambilan data Order Payments
+def create_orders_items_df(df):
+    orders_items_df = df.groupby('product_id')['order_id'].nunique().sort_values(ascending=False).head(5)
 
-### Assessing Data
-"""
+    return orders_items_df
 
-customers_df.info()
+def create_orders_seller_df(df):
+    orders_seller_df = df.groupby('seller_id')['order_id'].nunique().sort_values(ascending=False).head(5)
 
-print("Jumlah duplikasi: ", customers_df.duplicated().sum())
-customers_df.isna().sum()
+    return orders_seller_df
 
-orders_items_df.info()
+def create_orders_payments_df(df):
+    orders_payments_df = df.groupby('payment_type')['order_id'].nunique().sort_values(ascending=False).head(5)
 
-orders_items_df.isna().sum()
+    return orders_payments_df
 
-print("Jumlah duplikasi: ", orders_items_df.duplicated().sum())
-orders_items_df.describe()
 
-order_payments_df.info()
+# Load cleaned data
+df1 = pd.read_csv(r'D:\Submision_Analisis_Data\CSV\some_data.csv')
+df2 = pd.read_csv(r'D:\Submision_Analisis_Data\CSV\customers_dataset.csv')
 
-order_payments_df.isna().sum()
+all_df = pd.concat([df1, df2])
 
-print("Jumlah duplikasi: ", order_payments_df.duplicated().sum())
-order_payments_df.describe()
+# # Menyiapkan berbagai dataframe
+customers_state_df = create_customers_state_df(all_df)
+customers_city_df = create_customers_city_df(all_df)
+orders_items_df = create_orders_items_df(all_df)
+orders_seller_df = create_orders_seller_df(all_df)
+orders_payments_df = create_orders_payments_df(all_df)
 
-"""**Insight:**
-- Data costumers tidak ada keanehan
-- Data orders_items terdapat nilai maksimal yang lebih tinggi dibanding lainnya di price
-- Data orders_payments terdapat nilai maksimal yang lebih tinggi dibanding lainnya di payment value
+# Anggap customers_state_df adalah Series
+customers_state_df = pd.DataFrame({'State': customers_state_df.index, 'Number of Customers': customers_state_df.values})
+customers_city_df = pd.DataFrame({'City': customers_city_df.index, 'Number of Customers': customers_city_df.values})
+orders_items_df = pd.DataFrame({'Product ID': orders_items_df.index, 'Number of Orders': orders_items_df.values})
+orders_seller_df = pd.DataFrame({'Seller ID': orders_seller_df.index, 'Number of Orders': orders_seller_df.values})
+orders_payments_df = pd.DataFrame({'Payment Type': orders_payments_df.index, 'Number of Payment': orders_payments_df.values})
 
-### Cleaning Data
-"""
+st.title('SUBMISION')
+ 
+with st.sidebar:
+    
+    st.text('First Project SAP')
+    
+    st.text('E-commerce-public-dataset')
 
-orders_items_df[orders_items_df.price == orders_items_df.price.max()]
+    text = st.text_area('Please Comment About my project')
+    st.write('Good comment: ', text)
 
-orders_items_df.groupby(by="price").agg({
-    "order_item_id": "nunique",
-    "price": ["max", "min", "mean", "std"]
-})
+tab1, tab2 = st.tabs(["State", "City"])
+ 
+with tab1:
+    st.header("5 Daerah dengan Pembeli Terbanyak")
+    fig, ax = plt.subplots(figsize=(12, 6))
+    colors = ["#3ED0ED", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-orders_items_df.groupby(by="freight_value").agg({
-    "order_id": "nunique",
-    "freight_value": ["max", "min", "mean", "std"]
-})
+    sns.barplot(x='State', y='Number of Customers', data=customers_state_df, palette=colors)
+    plt.title('Customer Distribution by State')
+    plt.xlabel('State')
+    plt.ylabel('Number of Customers')
+    _ = plt.xticks(rotation=45, ha='right')
 
-print("freight_value", "sum")
+    st.pyplot(fig)
+ 
+with tab2:
+    st.header("5 Kota dengan Pembeli Terbanyak")
+    fig, ax = plt.subplots(figsize=(12, 6))
+    colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-order_payments_df[order_payments_df.payment_value == order_payments_df.payment_value.max()]
+    sns.barplot(x='City', y='Number of Customers', data=customers_city_df, palette=colors)
+    plt.title('Customer Distribution by City')
+    plt.xlabel('City')
+    plt.ylabel('Number of Customers')
+    _ = plt.xticks(rotation=45, ha='right')
 
-order_payments_df.groupby(by="payment_value").agg({
-    "order_id": "nunique",
-    "payment_value": ["max", "min", "mean", "std"]
-})
+    st.pyplot(fig)
+ 
 
-"""**Insight:**
-- Pengecekan dan Perapian Data Orders Items
-- Pengecekan dan Perapian Data Orders Payments
+with st.container():
+    plt.figure(figsize=(12, 6))
+    colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-## Exploratory Data Analysis (EDA)
 
-### Explore ...
-"""
+    st.header("Top 5 Produk Terlaris")
+    fig, ax = plt.subplots(figsize=(12, 6))
+    colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-some_df = pd.merge(
-    left=orders_items_df,
-    right=order_payments_df,
-    how="left",
-    left_on="order_id",
-    right_on="order_id"
-)
-some_df.head()
+    sns.barplot(x='Product ID', y='Number of Orders', data=orders_items_df, palette=colors)
+    plt.title('TOP Produk')
+    plt.xlabel('Product ID')
+    plt.ylabel('Number of Orders')
+    _ = plt.xticks(rotation=45, ha='right')
 
-some_df.groupby(by=["seller_id", "product_id"]).agg({
-    "order_item_id": "sum",
-    "price": "sum"
-})
+    st.pyplot(fig)
 
-some_df.groupby(by=["payment_type", "order_id"]).agg({
-        "payment_installments": "sum",
-        "payment_value": "sum"
-})
+with st.container():
+    plt.figure(figsize=(12, 6))
+    colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-some_df.groupby(by=["payment_type"]).agg({
-        "price": "sum",
-        "payment_value": "sum"
-})
 
-some_df.to_csv("some_data.csv", index=False)
+    st.header("Top 5 ID Seller dengan Penjualan Terbanyak")
+    fig, ax = plt.subplots(figsize=(12, 6))
+    colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-customers_df.describe(include="all")
+    sns.barplot(x='Seller ID', y='Number of Orders', data=orders_seller_df, palette=colors)
+    plt.title('TOP Saller Distribution')
+    plt.xlabel('Seller ID')
+    plt.ylabel('Number of Orders')
+    _ = plt.xticks(rotation=45, ha='right')
 
-customers_df.sample(5)
+    st.pyplot(fig)
 
-customers_df.groupby(by="customer_state").customer_id.nunique().sort_values(ascending=False)
+with st.container():
+    plt.figure(figsize=(12, 6))
+    colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-customers_df.groupby(by="customer_city").customer_id.nunique().sort_values(ascending=False)
 
-orders_items_df.describe(include="all")
+    st.header("Top 5 Tipe Pembayaran Terbaik")
+    fig, ax = plt.subplots(figsize=(12, 6))
+    colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-orders_items_df.sample(5)
+    sns.barplot(x='Payment Type', y='Number of Payment', data=orders_payments_df, palette=colors)
+    plt.title('TOP Payment Type')
+    plt.xlabel('Payment Type')
+    plt.ylabel('Number of Payment')
+    _ = plt.xticks(rotation=45, ha='right')
 
-orders_items_df.groupby(by="product_id").order_item_id.nunique().sort_values(ascending=False)
+    st.pyplot(fig)
 
-orders_items_df.groupby(by="seller_id").order_item_id.nunique().sort_values(ascending=False)
 
-order_payments_df.describe(include="all")
-
-order_payments_df.sample(5)
-
-order_payments_df.groupby(by="payment_type").order_id.nunique().sort_values(ascending=False)
-
-some_df.to_csv("some_data.csv", index=False)
-
-"""**Insight:**
-- Mendapatkan 5 costumer state dan costumer city terbanyak
-- Mendapatkan produk yang terlaris dan penjual yang paling banyak pembeli
-- Mendapatkan metode pembayaran terbanyak
-
-## Visualization & Explanatory Analysis
-
-### Pertanyaan 1:
-"""
-
-state_counts = customers_df.groupby('customer_state')['customer_unique_id'].nunique().sort_values(ascending=False).head(5)
-
-plt.figure(figsize=(12, 6))
-colors = ["#3ED0ED", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-sns.barplot(x=state_counts.index, y=state_counts.values, palette=colors)
-plt.title('Customer Distribution by State')
-plt.xlabel('State')
-plt.ylabel('Number of Customers')
-_ = plt.xticks(rotation=45, ha='right')
-
-state_counts = customers_df.groupby('customer_city')['customer_unique_id'].nunique().sort_values(ascending=False).head(5)
-
-plt.figure(figsize=(12, 6))
-colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-sns.barplot(x=state_counts.index, y=state_counts.values, palette=colors)
-plt.title('Customer Distribution by City')
-plt.xlabel('City')
-plt.ylabel('Number of Customers')
-_ = plt.xticks(rotation=45, ha='right')
-
-"""### Pertanyaan 2:"""
-
-state_counts = orders_items_df.groupby('product_id')['order_id'].nunique().sort_values(ascending=False).head(5)
-
-plt.figure(figsize=(12, 6))
-colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-sns.barplot(x=state_counts.index, y=state_counts.values, palette=colors)
-plt.title('Product Sales Distribution')
-plt.xlabel('Product ID')
-plt.ylabel('Number of Orders')
-_ = plt.xticks(rotation=45, ha='right')
-
-"""### Pertanyaan 3:"""
-
-state_counts = orders_items_df.groupby('seller_id')['order_id'].nunique().sort_values(ascending=False).head(5)
-
-plt.figure(figsize=(12, 6))
-colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-sns.barplot(x=state_counts.index, y=state_counts.values, palette=colors)
-plt.title('TOP Saller Distribution')
-plt.xlabel('Seller ID')
-plt.ylabel('Number of Orders')
-_ = plt.xticks(rotation=45, ha='right')
-
-"""### Pertanyaan 4:"""
-
-state_counts = order_payments_df.groupby('payment_type')['order_id'].nunique().sort_values(ascending=False).head(5)
-
-plt.figure(figsize=(12, 6))
-colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-sns.barplot(x=state_counts.index, y=state_counts.values, palette=colors)
-plt.title('TOP Payment Type')
-plt.xlabel('Payment Type')
-plt.ylabel('Number of Payment')
-_ = plt.xticks(rotation=45, ha='right')
-
-"""**Insight:**
-- Menampilkan 5 daerah dengan pembeli terbanyak
-- Menampilkan 5 produk dengan pembelian terbanyak
-- Menampilkan 5 ID penjual barang terbanyak
-- Menampilkan 5 metode pembayaran
-
-## Conclusion
-
-- Costumer terbanyak berasal dari daerah SP yang paling banyak dari daerah Sao Paulo
-- Produk ID 9571759451b1d780ee7c15012ea109d4 memiliki penjualan terbanyak
-- Penjual dengan ID 2709af9587499e95e803a6498a5a56e9 memiliki penjualan terbanyak
-- Metode yang digunakan terbanyak dalam pembayaran menggunakan Credit Card
-"""
+st.caption('Create by SAP')

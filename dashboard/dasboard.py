064247115db8a636c5f77987af6f1a44cb11da5c
suryaadi45 <adisurya271@gmail.com>
2024-12-31 17:26:24 +0800

    Update dasboard.py

--- dashboard/dasboard.py
+++ dashboard/dasboard.py
@@ -1,3 +1,4 @@
+#import library
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -5,6 +6,7 @@ import streamlit as st
 
 sns.set(style='dark')
 
+#create data
 def create_customers_state_df(df):
     customers_state_df = df.groupby('customer_state')['customer_unique_id'].nunique().sort_values(ascending=False).head(5)
 
@@ -32,12 +34,12 @@ def create_orders_payments_df(df):
 
 
 # Load cleaned data
-df1 = pd.read_csv(r'https://raw.githubusercontent.com/suryaadi45/Submision/refs/heads/main/dashboard/some_data.csv')
-df2 = pd.read_csv(r'https://raw.githubusercontent.com/suryaadi45/Submision/refs/heads/main/dashboard/customers_data.csv')
+df1 = pd.read_csv('dashboard/customers_data.csv')
+df2 = pd.read_csv('dashboard/some_data.csv')
 
 all_df = pd.concat([df1, df2])
 
-# # Menyiapkan berbagai dataframe
+# Menyiapkan berbagai dataframe
 customers_state_df = create_customers_state_df(all_df)
 customers_city_df = create_customers_city_df(all_df)
 orders_items_df = create_orders_items_df(all_df)
@@ -51,42 +53,26 @@ orders_items_df = pd.DataFrame({'Product ID': orders_items_df.index, 'Number of 
 orders_seller_df = pd.DataFrame({'Seller ID': orders_seller_df.index, 'Number of Orders': orders_seller_df.values})
 orders_payments_df = pd.DataFrame({'Payment Type': orders_payments_df.index, 'Number of Payment': orders_payments_df.values})
 
-datetime_columns = ["shipping_limit_date",]
-all_df.sort_values(by="shipping_limit_date", inplace=True)
-all_df.reset_index(inplace=True)
-
-for column in datetime_columns:
-    all_df[column] = pd.to_datetime(all_df[column])
-
-# Filter data
-min_date = all_df["shipping_limit_date"].min()
-max_date = all_df["shipping_limit_date"].max()
-
 st.title('SUBMISION')
- 
-with st.sidebar:
-    
-    st.text('First Project SAP')
-    
-    st.text('E-commerce-public-dataset')
 
-    # Mengambil start_date & end_date dari shipping_limit_date
-    start_date, end_date = st.date_input(
-        label='Waktu yang diinginkan',min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
+# menampilkan sidebar
+with st.sidebar:
+    st.text('First Project SAP')
+    st.text('E-commerce-public-dataset')
+    option = st.selectbox(
+        label="Data Apa yang ingin ditampilkan : ",
+        options=('Daerah dengan pembelian terbanyak', 
+                'Kota dengan pembelian terbanyak',
+                'Produk yang banyak terjual',
+                'Penjual dengan penjualan terbanyak',
+                'Metode pembayaran yang sering digunakan'
+                )
     )
 
-    main_df = all_df[(all_df["shipping_limit_date"] >= str(start_date)) & 
-                    (all_df["shipping_limit_date"] <= str(end_date))]
-
-    text = st.text_area('Please Comment About my project')
-    st.write('Good comment: ', text)
-
-tab1, tab2 = st.tabs(["State", "City"])
- 
-with tab1:
-    st.header("5 Daerah dengan Pembeli Terbanyak")
+ #opsi yang ada pada sidebar beserta logikanya   
+if option == 'Daerah dengan pembelian terbanyak':
+    
+    st.header("Top 5 Daerah dengan Pembeli Terbanyak")
     fig, ax = plt.subplots(figsize=(12, 6))
     colors = ["#3ED0ED", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -97,9 +83,14 @@ with tab1:
     _ = plt.xticks(rotation=45, ha='right')
 
     st.pyplot(fig)
- 
-with tab2:
-    st.header("5 Kota dengan Pembeli Terbanyak")
+    
+    # menambah keterangan
+    st.write("Dari diagram diatas dapat disimpulkan bahwa pembeli terbayak berasal dari daerah SP sebanyak 41746 pelanggan.")
+
+#opsi yang ada pada sidebar beserta logikanya   
+if option == 'Kota dengan pembelian terbanyak':
+    
+    st.header("Top 5 Kota dengan pembelian terbanyak")
     fig, ax = plt.subplots(figsize=(12, 6))
     colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -110,9 +101,13 @@ with tab2:
     _ = plt.xticks(rotation=45, ha='right')
 
     st.pyplot(fig)
- 
+    
+    # menambah keterangan
+    st.write("Kota dengan pembelian terbanyak didominasi oleh kota Sao Paulo sebanyak 16% dari total kota lainnya yang membuat kota tersebut mendominasi dalam pembelian produk")
 
-with st.container():
+ #opsi yang ada pada sidebar beserta logikanya   
+if option == 'Produk yang banyak terjual':
+    
     plt.figure(figsize=(12, 6))
     colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -128,8 +123,13 @@ with st.container():
     _ = plt.xticks(rotation=45, ha='right')
 
     st.pyplot(fig)
+    
+    # menambah keterangan
+    st.write("Dari diagram diatas dapat disimpulkan bahwa produk yang paling banyak terjual adalah yang memiliki kode 99a4788cb24856965c36a24e339b6058 sebanyak 467 item.")
 
-with st.container():
+ #opsi yang ada pada sidebar beserta logikanya   
+if option == 'Penjual dengan penjualan terbanyak':
+    
     plt.figure(figsize=(12, 6))
     colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -145,8 +145,13 @@ with st.container():
     _ = plt.xticks(rotation=45, ha='right')
 
     st.pyplot(fig)
+    
+    # menambah keterangan
+    st.write("Dari diagram diatas dapat disimpulkan bahwa penjual terbanyak memiliki id 6560211a19b47992c3666cc44a7e94c0 sebanyak 1854 item.")
 
-with st.container():
+ #opsi yang ada pada sidebar beserta logikanya   
+if option == 'Metode pembayaran yang sering digunakan':
+    
     plt.figure(figsize=(12, 6))
     colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -162,6 +167,8 @@ with st.container():
     _ = plt.xticks(rotation=45, ha='right')
 
     st.pyplot(fig)
-
+    
+    # menambah keterangan
+    st.write("Dari diagram diatas dapat disimpulkan bahwa  metode pembayaran terbanyak menggunakan credit_card sebanyak 76505 kali.")
 
 st.caption('Create by SAP')

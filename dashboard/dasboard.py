6027a7e4e44f59a3a8dd0f197366129cdad087ac
suryaadi45 <adisurya271@gmail.com>
2025-01-01 14:21:04 +0800

    Update dasboard.py

--- dashboard/dasboard.py
+++ dashboard/dasboard.py
@@ -34,24 +34,33 @@ def create_orders_payments_df(df):
 
 
 # Load cleaned data
-df1 = pd.read_csv('dashboard/customers_data.csv')
-df2 = pd.read_csv('dashboard/some_data.csv')
+df1 = pd.read_csv(r'D:\Submision_Analisis_Data\CSV\some_data.csv')
+df2 = pd.read_csv(r'D:\Submision_Analisis_Data\CSV\customers_data.csv')
 
 all_df = pd.concat([df1, df2])
+
+all_df["shipping_limit_date"] = pd.to_datetime(all_df["shipping_limit_date"])
+
+datetime_columns = ["shipping_limit_date"]
+all_df.sort_values(by="shipping_limit_date", inplace=True)
+all_df.reset_index(inplace=True)
+
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+# Filter data
+min_date = all_df["shipping_limit_date"].min()
+max_date = all_df["shipping_limit_date"].max()
 
 # Menyiapkan berbagai dataframe
 customers_state_df = create_customers_state_df(all_df)
 customers_city_df = create_customers_city_df(all_df)
-orders_items_df = create_orders_items_df(all_df)
-orders_seller_df = create_orders_seller_df(all_df)
-orders_payments_df = create_orders_payments_df(all_df)
 
 # Anggap customers_state_df adalah Series
 customers_state_df = pd.DataFrame({'State': customers_state_df.index, 'Number of Customers': customers_state_df.values})
 customers_city_df = pd.DataFrame({'City': customers_city_df.index, 'Number of Customers': customers_city_df.values})
-orders_items_df = pd.DataFrame({'Product ID': orders_items_df.index, 'Number of Orders': orders_items_df.values})
-orders_seller_df = pd.DataFrame({'Seller ID': orders_seller_df.index, 'Number of Orders': orders_seller_df.values})
-orders_payments_df = pd.DataFrame({'Payment Type': orders_payments_df.index, 'Number of Payment': orders_payments_df.values})
+
+
 
 st.title('SUBMISION')
 
@@ -110,7 +119,20 @@ if option == 'Kota dengan pembelian terbanyak':
 
  #opsi yang ada pada sidebar beserta logikanya   
 if option == 'Produk yang banyak terjual':
+
+    # Mengambil start_date & end_date dari shipping_limit_date
+    start_date, end_date = st.date_input(
+        label='Waktu yang diinginkan',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+    main_df = all_df[(all_df["shipping_limit_date"] >= str(start_date)) & 
+                    (all_df["shipping_limit_date"] <= str(end_date))]
     
+    orders_items_df = create_orders_items_df(main_df)
+    orders_items_df = pd.DataFrame({'Product ID': orders_items_df.index, 'Number of Orders': orders_items_df.values})
+
     plt.figure(figsize=(12, 6))
     colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -126,13 +148,23 @@ if option == 'Produk yang banyak terjual':
     _ = plt.xticks(rotation=45, ha='right')
 
     st.pyplot(fig)
-    
-    # menambah keterangan
-    st.write("Dari diagram diatas dapat disimpulkan bahwa produk yang paling banyak terjual adalah yang memiliki kode 99a4788cb24856965c36a24e339b6058 sebanyak 467 item.")
 
  #opsi yang ada pada sidebar beserta logikanya   
 if option == 'Penjual dengan penjualan terbanyak':
+
+    # Mengambil start_date & end_date dari shipping_limit_date
+    start_date, end_date = st.date_input(
+        label='Waktu yang diinginkan',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+    main_df = all_df[(all_df["shipping_limit_date"] >= str(start_date)) & 
+                    (all_df["shipping_limit_date"] <= str(end_date))]
     
+    orders_seller_df = create_orders_seller_df(main_df)
+    orders_seller_df = pd.DataFrame({'Seller ID': orders_seller_df.index, 'Number of Orders': orders_seller_df.values})
+
     plt.figure(figsize=(12, 6))
     colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -149,12 +181,22 @@ if option == 'Penjual dengan penjualan terbanyak':
 
     st.pyplot(fig)
     
-    # menambah keterangan
-    st.write("Dari diagram diatas dapat disimpulkan bahwa penjual terbanyak memiliki id 6560211a19b47992c3666cc44a7e94c0 sebanyak 1854 item.")
-
  #opsi yang ada pada sidebar beserta logikanya   
 if option == 'Metode pembayaran yang sering digunakan':
     
+    # Mengambil start_date & end_date dari shipping_limit_date
+    start_date, end_date = st.date_input(
+        label='Waktu yang diinginkan',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+    main_df = all_df[(all_df["shipping_limit_date"] >= str(start_date)) & 
+                    (all_df["shipping_limit_date"] <= str(end_date))]
+    
+    orders_payments_df = create_orders_payments_df(main_df)
+    orders_payments_df = pd.DataFrame({'Payment Type': orders_payments_df.index, 'Number of Payment': orders_payments_df.values})
+
     plt.figure(figsize=(12, 6))
     colors = ["#13C7EB", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -171,7 +213,4 @@ if option == 'Metode pembayaran yang sering digunakan':
 
     st.pyplot(fig)
     
-    # menambah keterangan
-    st.write("Dari diagram diatas dapat disimpulkan bahwa  metode pembayaran terbanyak menggunakan credit_card sebanyak 76505 kali.")
-
 st.caption('Create by SAP')
